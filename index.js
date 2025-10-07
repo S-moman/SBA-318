baaf6e366e70a813bfdd5de7491e501934f9091c
Moe Moman <moemoman@moes-mac-mini.mynetworksettings.com>
2025-10-07 11:31:30 -0400

    update, connecting server

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 import express from 'express';
 const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('We are live!')
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port: ${port}, we are in buiness!`)
+})
