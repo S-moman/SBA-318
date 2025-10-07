b7a41d57fd63c475db9de7787a5f1a0c823062c2
Moe Moman <moemoman@moes-mac-mini.mynetworksettings.com>
2025-10-07 13:47:41 -0400

    update to code

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ import express from 'express';
 const app = express();
 // Listen to it on a port
 const port = 3000;
-// Imort users from users folder
+// Imort users & post from data folder
 import users from './data/users.js';
+import posts from './data/posts.js';
 // Turn infor into JSON
 app.use(express.json())
 
-// Middleware for the time
+// Middleware for the time & type of request
 app.use((req, res, next) => {
     const time = new Date();
     console.log( `------------------------
@@ -24,13 +25,20 @@ app.get('/', (req, res) => {
 });
 
 // Users path
-app.get('/users', (req, res) => {
-    res.send(`Display user info here:
-        ${users}`)
+app.get('/api/users', (req, res) => {
+    res.json(users);
+});
+
+// Posts path
+app.get('/api/posts', (req, res) => {
+    res.json(posts)
 })
 
 
-
+app.use((req, res) => {
+    res.status(404);
+    res.send('ERROR!! Page Not Found')
+})
 
 // Listening function
 app.listen(port, () => {
