ce3606820a9acb628f3c3765a15f04085829a265
Moe Moman <moemoman@moes-mac-mini.mynetworksettings.com>
2025-10-07 12:01:48 -0400

    update data

---
+++ data/users.js
@@ -0,0 +1,12 @@
+// Create a data base to hold user information
+
+const users = [
+    {
+        id: 1,
+        name: "test",
+        username: "test123",
+        email: "test@test.com",
+    }
+]
+// Export information
+export default users
--- index.js
+++ index.js
@@ -1,11 +1,38 @@
+// Download & import express
 import express from 'express';
+// Connect it to your app
 const app = express();
+// Listen to it on a port
 const port = 3000;
+// Imort users from users folder
+import users from './data/users.js';
+// Turn infor into JSON
+app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send('We are live!')
+// Middleware for the time
+app.use((req, res, next) => {
+    const time = new Date();
+    console.log( `------------------------
+        ${time.toLocaleTimeString()}: Received a ${req.method} request to ${req.url}.
+        `);
+        next()
 })
 
+// Home path
+app.get('/', (req, res) => {
+    res.send('We are live! Welcome to my API!!!')
+});
+
+// Users path
+app.get('/users', (req, res) => {
+    res.send(`Display user info here:
+        ${users}`)
+})
+
+
+
+
+// Listening function
 app.listen(port, () => {
     console.log(`Listening on port: ${port}, we are in buiness!`)
 })
